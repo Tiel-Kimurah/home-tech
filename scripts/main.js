93dec843d8ff7fde9e22401682acd00c02d81a61
Tiel <120792534+Tiel-Kimurah@users.noreply.github.com>
2024-03-12 11:43:33 -0300

    adicao de novo arquivo
    loja onlline

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+
+
+//versao mobile do cabecalho
+const menu = document.querySelector('.menu');
+const NavMenu = document.querySelector('.nav-menu');
+
+menu.addEventListener('click', ()=>{
+    menu.classList.toggle('ativo');
+    NavMenu.classList.toggle('ativo');
+});
